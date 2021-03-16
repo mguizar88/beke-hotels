560e03b1854d4bc814d4d12c56742738e43cafff
mguizar88 <mguizar@creanddo.com>
2021-03-16 10:41:07 -0500

    reserva

---
+++ src/templates/hotel-page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Layout from '../components/Layout'
+
+const HotelPageTemplate = () => (
+    <Layout>
+       
+    </Layout>
+)
+
+export default HotelPageTemplate
