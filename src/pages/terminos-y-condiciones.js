e7c977ccebb143208ff59999c28619e66d3db810
mguizar88 <mguizar@creanddo.com>
2021-06-29 16:24:00 -0500

    t&c

--- src/pages/terminos-y-condiciones.js
+++ src/pages/terminos-y-condiciones.js
@@ -6,21 +6,21 @@ const Page = () => {
         <Layout>
             <div className="bg-gradient-to-br from-beke to-gray-900">
                 <div className="max-w-screen-md mx-auto py-12 px-4">
-                    <h1 className="text-center text-white text-2xl sm:text-6xl mb-12">Terminos y condiciones</h1>
-                    <div className="shadow-lg rounded-xl mb-16 p-4 bg-beke text-white">
+                    <h1 className="text-center text-white text-2xl sm:text-6xl mb-12">Términos y condiciones</h1>
+                    <div className="shadow-lg rounded-xl mb-16 p-4 bg-gray-900 text-white">
                         <p>Totales en pesos mexicanos e impuestos incluidos.</p>
                         <p>Mejor precio garantizado: Evita cargos y comisiones de agencias.</p>
                         <p>Métodos de pago: transferencia, tarjeta y depósito.</p>
                         <p>Aparta con 50% y liquida al llegar.</p>
                         <p>Teléfono: (998) 272 2665</p>
                     </div>
-                    <div className="shadow-lg rounded-xl my-16 p-4 bg-beke text-white">
+                    <div className="shadow-lg rounded-xl my-16 p-4 bg-gray-900 text-white">
                         <h2>Tarifa Flex:</h2>
                         <p>
                             Porque sabemos que la vida nos sorprende, nuestra tarifa flex brinda la tranquilidad de poder modificar fácilmente tus fechas sin costo alguno, además, garantiza la tarifa más económica.
                         </p>
                     </div>
-                    <div className="shadow-lg rounded-xl my-16 p-4 bg-beke text-white">
+                    <div className="shadow-lg rounded-xl my-16 p-4 bg-gray-900 text-white">
                         <p>*Cambio de fecha, nombre y tipo de habitación hasta con 5 días de anticipación sin costo.</p>
                         <p>*Vigencia de 365 días y sujeta a disponibilidad.</p>
                         <p>*Entrada y salida preferencial.</p>
