0cfa5ed741578437a82f3c25c5ec058bc5f585e3
mguizar88 <mguizar@creanddo.com>
2021-11-23 16:57:19 -0500

    Add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -92,5 +92,30 @@ module.exports = {
         pixelId: "524476572214880",
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "294459563",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "hotel-pier-bacalar.bekehotels.com",
+        // defaults to false
+        enableWebVitalsTracking: true,
+      },
+    },
   ],
 }
