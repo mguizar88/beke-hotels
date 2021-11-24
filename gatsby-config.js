8b118f8b967266707232bd0b4fabafc17ff4dbd8
mguizar88 <mguizar@creanddo.com>
2021-11-23 19:59:31 -0500

    g tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -96,7 +96,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "294459563",
+        trackingId: "G-H4WEC6N8Y0",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
         // Setting this parameter is optional
